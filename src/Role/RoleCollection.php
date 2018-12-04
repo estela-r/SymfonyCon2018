795e8e31e4eb2ec4b0309a4a3ab7cbfc8dfab2aa
MichaelC <m@michaelcullum.com>
2018-12-04 14:26:59 +0000

    Update collection to be immutable

--- src/Role/RoleCollection.php
+++ src/Role/RoleCollection.php
@@ -3,10 +3,16 @@ declare(strict_types=1);
 
 namespace App\Role;
 
+use Closure;
 use Doctrine\Common\Collections\ArrayCollection;
 
-class RoleCollection extends ArrayCollection
+class RoleCollection
 {
+    /**
+     * @var Role[]
+     */
+    private $elements = [];
+
     public function add($element)
     {
         $this->ensureIsRole($element);
@@ -15,18 +21,19 @@ class RoleCollection extends ArrayCollection
             return;
         }
 
-        return parent::add($element);
+        return new RoleCollection(array_merge($this->elements, [$element]));
     }
 
     public function set($key, $value)
     {
         $this->ensureIsRole($value);
-        parent::set($key, $value);
+
+        return new RoleCollection(array_merge($this->elements, [$key => $value]));
     }
 
     public function __construct(array $elements = [])
     {
-        parent::__construct($elements);
+        $this->elements = $elements;
         $this->forAll([$this, 'ensureIsRole']);
     }
 
@@ -36,4 +43,22 @@ class RoleCollection extends ArrayCollection
             throw new \TypeError('In a role collection, only have roles!');
         }
     }
+
+    public function forAll(Closure $p)
+    {
+        foreach ($this->elements as $key => $element) {
+            if ( ! $p($key, $element)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function contains($element)
+    {
+        return in_array($element, $this->elements, true);
+    }
+
+    /** Other collection-y methods here :) */
 }
